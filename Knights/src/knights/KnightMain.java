1957801e037dc43e61e41ca732d32685dbb7010e
leonliu06 <leonliu06@gmail.com>
2017-03-29 14:56:54 +0800

    aop

--- Knights/src/knights/KnightMain.java
+++ Knights/src/knights/KnightMain.java
@@ -3,13 +3,16 @@ package knights;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
-public class KnightMain {
-  public static void main(String[] args) {
-    ApplicationContext context = 
-        new ClassPathXmlApplicationContext("knights.xml"); //<co id="co_loadKnightContext"/>
-    
-    Knight knight = (Knight) context.getBean("knight"); //<co id="co_getBeanKnight"/>
-    
-    knight.embarkOnQuest();//<co id="co_useKnight"/>
-  }
+public class KnightMain{
+	
+	public static void main(String[] args){
+	  
+	  ApplicationContext context = new ClassPathXmlApplicationContext("knights.xml");
+	  
+	  Knight knight = (Knight) context.getBean("knight");
+	  
+	  knight.embarkOnQuest();
+	  
+	}
+	
 }
